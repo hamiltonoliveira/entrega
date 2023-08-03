1950dcd5b818c6148a134936f3886ca0684a87ec
Hamilton Vale <hamilton.valnet@gmail.com>
2023-08-03 10:46:47 -0300

    validar cpf

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Dto;
 using Domain.Enums;
+using Domain.Validation;
 using FluentValidation;
 using System.Text.Json.Serialization;
 
@@ -15,6 +16,12 @@ namespace Domain.Entities
         public string? Email { get; set; }
         public string? Cpf { get; set; }
         public TipoFuncionario Role { get; set; }
+        protected User() { }
+
+        public User(string Cpf)
+        {
+            SetCpf(Cpf);
+        }
 
         public void SetUserName(string email)
         {
@@ -25,6 +32,20 @@ namespace Domain.Entities
             }
         }
 
+        public void SetCpf(string cpf)
+        {
+            if (AtribuirCpf(cpf))
+            {
+                this.Cpf = cpf;
+            }
+        }
+        private bool AtribuirCpf(string cpfNumero)
+        {
+            var cpf = new Cpf(cpfNumero);
+            if (!cpf.Validar()) return false;
+            return true;
+        }
+
         public class UserValidation : AbstractValidator<UserDTO>
         {
             public UserValidation()
@@ -32,6 +53,7 @@ namespace Domain.Entities
                 RuleFor(x => x.UserName).NotNull().NotEmpty().WithMessage("O campo Name não pode ser nulo");
                 RuleFor(x => x.Password).NotEmpty().WithMessage("O campo Password não pode ser nulo");
                 RuleFor(x => x.Email).NotNull().NotEmpty().WithMessage("O campo de Email é obrigatório.").EmailAddress().WithMessage("O campo de email não possui um formato válido.");
+                RuleFor(x => x.Cpf).NotNull().NotEmpty().WithMessage("O campo de Cpf é obrigatório.");
                 RuleFor(x => x.Role).NotNull().NotEmpty().WithMessage("O campo Role não pode ser nulo");
             }
         }
